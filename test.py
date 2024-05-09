1463cdc747e75494878eb487f6d5a150c140b222
Eason HM <50075926+RUSRUSHB@users.noreply.github.com>
2024-05-09 11:37:33 +0800

    Create test.py

---
+++ test.py
@@ -0,0 +1,28 @@
+import cv2
+import numpy as np
+
+img = cv2.imread("input/Trefoil.png")
+
+# threshold the image
+gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+_, gray = cv2.threshold(gray, 127, 255, cv2.THRESH_BINARY)
+# invert black and white
+gray = cv2.bitwise_not(gray)
+
+
+
+# show the image
+cv2.imshow("gray", gray)
+cv2.waitKey(0)
+
+
+# connected component analysis
+num_labels, labels, stats, centroids = cv2.connectedComponentsWithStats(gray, connectivity=4)
+
+print(f'num_labels: {num_labels}')
+print(f'max of labels: {np.max(labels)}')
+
+print(f'stats: {stats}')
+print(f'centroids: {centroids}')
+
+
