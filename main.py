ac45a0fc3184131c620d2fb4d455c659cbebca16
Eason HM <50075926+RUSRUSHB@users.noreply.github.com>
2024-06-22 18:14:43 +0800

    Update README.md

--- main.py
+++ main.py
@@ -14,45 +14,39 @@ import re
 
 img = load_image("img/rolfsen_all/9_32.png")
 
-windowSizes = [27, 30, 24, 20, 16] # 尝试的windowSize备选值
-# windowSizes = [27]
-windowSize = windowSizes[0]  # 初始值为27
-attempts = 1  # 尝试次数
+# windowSizes = [27, 30, 24, 20, 16] # 尝试的windowSize备选值
 
-while True:  # 循环直到不再遇到异常或者没有更多备选的windowSize
-    try:
-        # visualize(img, 'Original Image')
+windowSize = 16
 
-        bi_img = binarize(img)
+# visualize(img, 'Original Image')
 
-        labels, component_sizes, output_img = segment_image(bi_img)
-        labels = arrange_labels(labels)
+bi_img = binarize(img)
+
+labels, component_sizes, output_img = segment_image(bi_img)
+labels = arrange_labels(labels)
+print(f'labels: {labels}')
+print(f'component_sizes: {component_sizes}')
+visualize_labels_with_random_color_and_text(labels)
+
+# save_image(output_img, 'output/3_1_segment.png')
+
+separate_labels = separate_labels(labels)
+show_each_components(separate_labels)
+
+classification = classify_segments(separate_labels)
+unifyLabels = unify_non_line_segments(labels, classification)
+visualize_labels_with_random_color_and_text(unifyLabels)
+crossings = process_image(unifyLabels, windowSize)
+visualize_labels_with_random_color_and_text_and_crossingWindow(
+    unifyLabels, crossings, windowSize)
+
+print(f'classification: {classification}')
+print(f"the crossings are {crossings}")
+crossings_arr = extractLabelsFromCross_detect(crossings)
+print(f"crossings_arr: {crossings_arr}")
+unifyCrossings = unifyCrossings(crossings_arr)
+print(f"crossings_arr: {unifyCrossings}")
 
 
-        result = alex_polynomial(straighten(unifyCrossings(extractLabelsFromCross_detect(process_image(unify_non_line_segments(
-            labels, classify_segments(separate_labels(labels))), windowSize)))))
-        
-        
-        if attempts > 1:
-            print(
-                f"is {result}. ({attempts} attempts)")
-        else:
-            print(f"is {result})")
-        attempts = 1
-
-        # print(
-        #     f"the knot detection result for {filename} has been saved in the folder output")
-        # print(
-        #     f"the alex polynomial result for {filename} has been saved in the folder output")
-        break  # 成功处理该文件，退出循环
-    except Exception as e:
-        # print(f"Error processing {filename} with windowSize {windowSize}: {e}")
-        # 如果遇到异常，尝试下一个windowSize，如果已经没有备选值了，则退出循环
-        if windowSize != windowSizes[-1]:
-            attempts+=1
-            windowSize = windowSizes[windowSizes.index(windowSize) + 1]
-        else:
-            print(f'All attempts failed')
-            # save this picture to the error folder
-            # save_image(bi_img, f'output/error/advance_cross_4/{filename}')
-            break
+straightened_data = straighten(unifyCrossings)
+print("The result is: ", alex_polynomial(straightened_data)[0])
